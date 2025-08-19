db965d76bcc18aa476236e739e51af4b57b4b44f
devy-wtf <dasolisg.2005fwd@gmail.com>
2025-08-19 11:31:36 -0600

    update4

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,15 @@
-const tablero = document.getElementById('tablero')
+let tablero = array(9)
 
-const estado = {
-    tablero: Array(9),
-    turno: "x",
-    modoCPU: false,
-    activo: true,
-}
+let jugadorActual = 1
+let juegoTerminado = false
+
 
 const ganadores = [
     [0,1,2], [3,4,5], [6,7,8],
-    [0,3,6], [1,4,7], [3,5,8],
+    [0,3,6], [1,4,7], [2,5,8],
     [0,4,8], [2,4,6],
 ]
+
+
 
 
