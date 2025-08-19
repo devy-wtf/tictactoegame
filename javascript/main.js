2922776687c46e58d5812cf6d4dd5ada43ff348d
devy-wtf <dasolisg.2005fwd@gmail.com>
2025-08-19 15:55:41 -0600

    update6

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,55 @@
-let tablero = array(9)
+let tablero = array(9).fill("")
 
-let jugadorActual = 1
+let jugadorActual = "X"
 let juegoTerminado = false
+let turnoCpu =  false
 
 
-const ganadores = [
+const combGanadoras = [
     [0,1,2], [3,4,5], [6,7,8],
     [0,3,6], [1,4,7], [2,5,8],
     [0,4,8], [2,4,6],
 ]
 
+
+let gameOver = false
+
+function statusJuego(){
+    for (let combinación of combGanadoras){
+        const [a, b, c] = combinación
+        if(
+            tablero[a] !== "" &&
+            tablero[b] === tablero[b] &&
+            tablero[c] === tablero[c]
+
+
+        )}
+    
+}
+
+
+
+
+const celdas = document.querySelectorAll('.celda')
+
+celdas.forEach(celda =>{
+    const index = parseInt(celda.dataset.index)
+    cell.addEventListener("click", () => movimiento(index, celda))
+}
+
+)
+
+
+function movimiento(index, celdaElement){
+    if(tablero[index] !== "") return;
+
+    tablero[index] = jugadorActual
+    celdaElement.textContent = jugadorActual
+    celdaElement.classList.add(`marcado-${currentPlayer.toLowerCase()}`);
+
+
+    jugadorActual = jugadorActual === "X" ? "O" : "X"
+}
 
 
 
